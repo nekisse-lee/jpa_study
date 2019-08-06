4e2f4df791825e7dc9853149a337745f2cbda8ed
nekisse <lsh891224@gmail.com>
2019-08-06 14:01:19 +0900

    jpa-study  h2db 설정,  jpa, jpa query

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,16 +2,47 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
         EntityManager em = emf.createEntityManager();
-        //code
 
-        em.clear();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        //code
+        try {
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("HelloB");
+
+//            Member findMember = em.find(Member.class, 1L);
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                .setFirstResult(5)
+                .setMaxResults(8)
+                .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.getName() = " + member.getName());
+            }
+
+//            System.out.println("findMember.getId = " + findMember.getId());
+//            System.out.println("findMember.getName() = " + findMember.getName());
+//            findMember.setName("HelloJPA");
+
+            //            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
 
         emf.close();
     }
