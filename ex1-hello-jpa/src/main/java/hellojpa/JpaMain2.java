e8ecd7b2643b92addfd261876d550a5a404400be
nekisse <lsh891224@gmail.com>
2019-08-06 15:33:24 +0900

    jpa-study  영속성 컨텍스트 example

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain2.java
@@ -0,0 +1,44 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain2 {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        //code
+        try {
+            //비영속
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("HelloJPA");
+
+            //영속
+//            System.out.println(" === BEFORE ===");
+//            em.persist(member);
+//            System.out.println(" === AFTER ===");
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
+            System.out.println("findMember1==findMember2 = " + (findMember1==findMember2));
+//            System.out.println("findMember.getId() = " + findMember.getId());
+//            System.out.println("findMember.getName() = " + findMember.getName());
+            // 트랜잭션에 커밋시 쿼리 실행.
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
