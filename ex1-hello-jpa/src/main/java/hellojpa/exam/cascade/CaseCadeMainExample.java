762a1af183d4630568e2164e72a34ff6c0bf74f5
nekisse <lsh891224@gmail.com>
2019-08-13 13:50:56 +0900

    hello-jpa - 영속성 전이(CASCADE) 와 고아객체

--- ex1-hello-jpa/src/main/java/hellojpa/exam/cascade/CaseCadeMainExample.java
+++ ex1-hello-jpa/src/main/java/hellojpa/exam/cascade/CaseCadeMainExample.java
@@ -24,11 +24,20 @@ public class CaseCadeMainExample {
 
 
             em.persist(parent);
+    //            em.persist(child1);
+    //            em.persist(child2);
+
 //            em.persist(child1);
 //            em.persist(child2);
 
-            tx.commit();
+            em.flush();
+            em.clear();
 
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);
+//            em.remove(findParent);
+
+            tx.commit();
         } catch (Exception e) {
             tx.rollback();
             e.printStackTrace();
