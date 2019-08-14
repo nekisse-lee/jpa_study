1b010cb223a2758da191e9a0406fa548c4b0f780
nekisse <lsh891224@gmail.com>
2019-08-14 16:13:15 +0900

    jpashop - 실전예제6  값 타입 매핑

--- jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -10,13 +10,45 @@ public class Delivery extends BaseEntity{
     private Long id;
 
 
-    private String city;
-    private String street;
-    private String zipcode;
+    @Embedded
+    private Address address;
+
     @Enumerated(EnumType.STRING)
     private DeliveryStatus Status;
 
 
     @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public DeliveryStatus getStatus() {
+        return Status;
+    }
+
+    public void setStatus(DeliveryStatus status) {
+        Status = status;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
