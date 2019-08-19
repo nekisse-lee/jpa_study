a1960f296989afaa492e34df67d3d0d9e996dcd5
nekisse <lsh891224@gmail.com>
2019-08-19 16:37:58 +0900

    jpql - 벌크연산
    *벌크연산 후 영속성 컨텍스트 초기화

---
+++ jpql/src/main/java/jpql/JpqlMain9.java
@@ -0,0 +1,83 @@
+package jpql;
+
+
+import jpql.domain.Member;
+import jpql.domain.Team;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpqlMain9 {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        //code
+        try {
+
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setAge(0);
+            member1.setTeam(teamA);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setAge(0);
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setAge(0);
+            member3.setTeam(teamB);
+            em.persist(member3);
+
+
+//            em.flush();
+//            em.clear();
+//            FLUSH 자동 호출, commit, query 시점,   이 아니면 강제 flush()
+            int resultCount = em.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
+
+            em.clear();  //벌크연산 후 영속성 컨텍스트 초기화!
+
+            Member findMember = em.find(Member.class, member1.getId());
+
+            System.out.println("findMember = " + findMember.getAge());
+
+//            System.out.println("resultCount = " + resultCount);
+//
+//            System.out.println("member1.getAge() = " + member1.getAge());
+//            System.out.println("member2.getAge() = " + member2.getAge());
+//            System.out.println("member3.getAge() = " + member3.getAge());
+
+            tx.commit();
+        } catch (Exception e) {
+
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+}
